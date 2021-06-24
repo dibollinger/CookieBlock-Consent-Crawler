00592822dea9ac94660982718ef7a3733f6c8352
Dino Bollinger <dino.bollinger@gmail.com>
2021-06-24 16:49:11 +0200

    Fix some SQL commands

--- database_processing/sql_commands/commands/consent_crawl_with_website.sql
+++ database_processing/sql_commands/commands/consent_crawl_with_website.sql
@@ -1,5 +1,5 @@
 -- Create a view of the consent crawl results where the website is also listed.
-CREATE VIEW consent_data_with_cmp AS
+CREATE VIEW IF NOT EXISTS consent_data_with_cmp AS
 SELECT sv.site_url, v.cmp_type, c.* 
 FROM consent_data c
 JOIN consent_crawl_results v ON v.visit_id == c.visit_id
--- database_processing/sql_commands/commands/javascript_cookies_with_url.sql
+++ database_processing/sql_commands/commands/javascript_cookies_with_url.sql
@@ -1,5 +1,5 @@
 -- Create a view of the javascript cookie results with corresponding URL
-CREATE VIEW javascript_cookies_with_url AS
+CREATE VIEW IF NOT EXISTS javascript_cookies_with_url AS
 SELECT sv.site_url, c.* 
 FROM javascript_cookies c
 JOIN site_visits sv ON sv.visit_id == c.visit_id
